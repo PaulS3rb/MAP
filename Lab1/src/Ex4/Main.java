986e11c4b614d732083d5745f5f4cf8f522704b3
PaulS3rb <paulserb2@gmail.com>
2024-10-15 21:58:12 +0300

    added CheapestKeyboard method

--- Lab1/src/Ex4/Main.java
+++ Lab1/src/Ex4/Main.java
@@ -1,4 +1,12 @@
 package Ex4;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Shop shop = new Shop();
+        int[] Keyboards={40,35,70,15,45,15};
+        int[] USBs={20,15,40,15};
+        System.out.println(shop.CheapestKeyboard(Keyboards));
+    }
 }
--- Lab1/src/Ex4/Shop.java
+++ Lab1/src/Ex4/Shop.java
@@ -1,4 +1,13 @@
 package Ex4;
 
 public class Shop {
+
+    public int CheapestKeyboard(int[] Keyboards)
+    {   int min=Integer.MAX_VALUE;
+        for(int i = 0; i < Keyboards.length; i++)
+            if(Keyboards[i]<min)
+                min = Keyboards[i];
+        return min;
+    }
+
 }
