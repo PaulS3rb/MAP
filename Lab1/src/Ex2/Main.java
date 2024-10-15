e3a4be0d420051a8f972e8bfaf9b0f7910511c71
PaulS3rb <paulserb2@gmail.com>
2024-10-15 18:54:46 +0300

    Added FindMaximum, FindMinimum and MaxSum method

--- Lab1/src/Ex2/Main.java
+++ Lab1/src/Ex2/Main.java
@@ -1,4 +1,12 @@
 package Ex2;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Numbers n = new Numbers();
+        int [] numbers={6,4,12,3,12,3};
+        System.out.println(n.FindMaximum(numbers));
+        System.out.println(n.FindMinimum(numbers));
+        System.out.println(n.MaxSum(numbers));
+    }
 }
--- Lab1/src/Ex2/Numbers.java
+++ Lab1/src/Ex2/Numbers.java
@@ -1,4 +1,46 @@
 package Ex2;
 
 public class Numbers {
+
+    public int FindMaximum(int[] numbers)
+    {
+        int max=-1;
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]>max)
+                max=numbers[i];
+        }
+        return max;
+    }
+
+    public int FindMinimum(int [] numbers)
+    {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<numbers.length;i++)
+        {
+            if(numbers[i]<min)
+                min=numbers[i];
+        }
+        return min;
+    }
+
+    public int MaxSum(int [] numbers)
+    {
+        int sum=0;
+        boolean found=false;
+        int minnumber=FindMinimum(numbers);
+        for(int i=0;i<numbers.length;i++) {
+            if (numbers[i] == minnumber) {
+                if (!found) {
+                    found = true;
+                    continue;
+                }
+            }
+
+            sum+=numbers[i];
+
+        }
+        return sum;
+    }
+
 }
