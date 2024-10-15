c9582ed21e0fe1e47290fbcda69944673b66d9e6
PaulS3rb <paulserb2@gmail.com>
2024-10-15 18:35:43 +0300

    modified array value

--- Lab1/src/Ex1/Main.java
+++ Lab1/src/Ex1/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Grades object = new Grades();
-        int[] grades=new int[]{17, 29, 66 , 45, 37, 80, 93,97};
+        int[] grades=new int[]{17, 29, 66 , 45, 37, 80, 93,98};
         System.out.println(Arrays.toString(object.insufficientGrade(grades)));
         System.out.println(object.AverageGrade(grades));
         System.out.println(Arrays.toString(object.AdjustGrades(grades)));
