8760df41d05fe7712023c3ad892b41f44382464e
PaulS3rb <paulserb2@gmail.com>
2024-10-15 16:07:08 +0300

    solved Exercise 1 a)

--- Lab1/src/Ex1/Grades.java
+++ Lab1/src/Ex1/Grades.java
@@ -1,4 +1,26 @@
 package Ex1;
 
 public class Grades {
+
+    public int[] insufficientGrade(int[] grades)
+    {
+        int [] insufficientGrades = new int[0];
+        for(int i = 0; i < grades.length; i++)
+        {
+            if (grades[i] < 40)
+            {
+                insufficientGrades = AddToArray(insufficientGrades,grades[i]);
+            }
+        }
+        return insufficientGrades;
+    }
+
+    private int[] AddToArray(int[] array, int grade)
+    {
+        int [] newArray = new int[array.length + 1];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[array.length] = grade;
+        return newArray;
+    }
+
 }
--- Lab1/src/Ex1/Main.java
+++ Lab1/src/Ex1/Main.java
@@ -1,10 +1,15 @@
 package Ex1;
 
+import java.util.Arrays;
+
 import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Grades object = new Grades();
+        int[] grades=new int[]{17, 29, 66 , 45, 37, 80, 92};
+        System.out.println(Arrays.toString(object.insufficientGrade(grades)));
     }
 
 }
