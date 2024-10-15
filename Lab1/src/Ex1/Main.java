b37fc08647bc077569e6b996584d5faff87e3296
PaulS3rb <paulserb2@gmail.com>
2024-10-15 22:26:16 +0300

    added more comments in main

--- Lab1/src/Ex1/Main.java
+++ Lab1/src/Ex1/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Grades object = new Grades();
         int[] grades=new int[]{17, 29, 66 , 45, 37, 80, 93,98};
-        System.out.println(Arrays.toString(object.insufficientGrade(grades)));
-        System.out.println(object.AverageGrade(grades));
-        System.out.println(Arrays.toString(object.AdjustGrades(grades)));
-        System.out.println(object.MaximalGrade(grades));
+        System.out.println("Insufficient Grades: "+Arrays.toString(object.insufficientGrade(grades)));
+        System.out.println("Average Grade: "+ object.AverageGrade(grades));
+        System.out.println("Modified Grades: "+Arrays.toString(object.AdjustGrades(grades)));
+        System.out.println("Biggest Modified Grade: "+ object.MaximalGrade(grades));
 
 
     }
--- Lab1/src/Ex2/Main.java
+++ Lab1/src/Ex2/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Numbers n = new Numbers();
         int [] numbers={6,4,12,3,12,3};
-        System.out.println(n.FindMaximum(numbers));
-        System.out.println(n.FindMinimum(numbers));
-        System.out.println(n.MaxSum(numbers));
-        System.out.println(n.MinSum(numbers));
+        System.out.println("Biggest number: " +n.FindMaximum(numbers));
+        System.out.println("Smallest number: "+n.FindMinimum(numbers));
+        System.out.println("Biggest sum of all numbers except one: "+n.MaxSum(numbers));
+        System.out.println("Smallest sum of all numbers except one: "+ n.MinSum(numbers));
     }
 }
