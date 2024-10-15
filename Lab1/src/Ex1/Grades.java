98b76941e26927d131630f0a7a09ffc642af1e0b
PaulS3rb <paulserb2@gmail.com>
2024-10-15 16:26:58 +0300

    added adjusted grades method

--- Lab1/src/Ex1/Grades.java
+++ Lab1/src/Ex1/Grades.java
@@ -2,6 +2,7 @@ package Ex1;
 
 public class Grades {
 
+
     public int[] insufficientGrade(int[] grades)
     {
         int [] insufficientGrades = new int[0];
@@ -15,6 +16,33 @@ public class Grades {
         return insufficientGrades;
     }
 
+    public int[] AverageGrade(int[] grades)
+    {
+        return grades;
+    }
+
+    public int[] AdjustGrades(int [] grades)
+    {
+
+        for(int i = 0; i < grades.length; i++)
+        {
+            if(grades[i] < 38)
+            {
+                continue;
+            }
+            else
+            {
+                if(grades[i]%5>=3)
+                {
+                    while (grades[i] % 5 != 0)
+                        grades[i]++;
+
+                }
+
+            }
+        }
+        return grades;
+    }
     private int[] AddToArray(int[] array, int grade)
     {
         int [] newArray = new int[array.length + 1];
--- Lab1/src/Ex1/Main.java
+++ Lab1/src/Ex1/Main.java
@@ -10,6 +10,7 @@ public class Main {
         Grades object = new Grades();
         int[] grades=new int[]{17, 29, 66 , 45, 37, 80, 92};
         System.out.println(Arrays.toString(object.insufficientGrade(grades)));
+        System.out.println(Arrays.toString(object.AdjustGrades(grades)));
     }
 
 }
