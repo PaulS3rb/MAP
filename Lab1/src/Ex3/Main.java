24bfc336c43dcbee14016e816efcc0cf51dfaac4
PaulS3rb <paulserb2@gmail.com>
2024-10-15 21:43:59 +0300

    added Ex4 components

--- Lab1/src/Ex3/Main.java
+++ Lab1/src/Ex3/Main.java
@@ -4,10 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         ArraysNumbers object = new ArraysNumbers();
-        int[] Array1=new int[]{1,0,2,9,3,4,2};
+        int[] Array1=new int[]{3,0,2,9,3,4,2};
         int[] Array2=new int[]{7,7,9,7,9,1,2};
-        System.out.println(Arrays.toString(object.sum(Array1,Array2)));
-        System.out.println(Arrays.toString(object.difference(Array1,Array2)));
-        System.out.println(Arrays.toString(object.Multiplication(Array1,3)));
+        System.out.println("Addition: "+Arrays.toString(object.sum(Array1,Array2)));
+        System.out.println("Subtraction: "+Arrays.toString(object.difference(Array1,Array2)));
+        System.out.println("Multiplication: "+Arrays.toString(object.multiplication(Array1,3)));
+        System.out.println("Division: "+Arrays.toString(object.division(Array1,2)));
     }
 }
